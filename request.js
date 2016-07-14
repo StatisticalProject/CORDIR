66bab6d300f658a520d21475e5ed699bff4a32ae
Sebastien DIAZ <sebastien.diaz@gmail.com>
2016-07-14 22:02:28 +0200

    ye

---
+++ request.js
@@ -0,0 +1,56 @@
+db.project.aggregate(
+    { 
+	$group : {_id : "$topics", total : { $sum : 1 },totalCost:{$sum : '$totalCost'},avgCost:{$avg : '$totalCost'},stdDevCost:{$stdDevPop : '$totalCost'} }
+    }
+  ,
+     {
+	$sort : {total : -1}
+     }).forEach(function(data){
+  print(data._id+","+data.total+","+data.totalCost+","+data.avgCost+","+data.stdDevCost);
+});
+db.project.aggregate(
+    { 
+	$group : {_id : "$programme"
+            ,count : { $sum : 1 }
+            ,totalCost:{$sum : '$totalCost'},avgTotalCost:{$avg : '$totalCost'},stdDevTotalCost:{$stdDevPop : '$totalCost'}
+            ,call:{$sum : '$call'},avgCall:{$avg : '$call'},stdDevCall:{$stdDevPop : '$call'}}
+    }
+  ,
+     {
+	$sort : {count : -1}
+     }
+     ,{
+    $lookup: {
+            from: "programme",
+            localField: "_id",
+            foreignField: "Code",
+            as: "programeDesc"
+        }
+},
+    { $redact: {
+        $cond: {
+           if: 
+                { $not: [ { $or:[
+                    {$eq: [ "$Language", "pl" ] },
+                    {$eq: [ "$Language", "en" ] },
+                    {$eq: [ "$Language", "it" ] },
+                    {$eq: [ "$Language", "de" ] },
+                    {$eq: [ "$Language", "es" ] }
+                    ]}
+                ]}
+           ,
+           then: "$$DESCEND",
+           else: "$$PRUNE"
+        }
+    }
+    }
+     )
+     
+     .forEach(function(data){
+  print(data._id +","+data.programeDesc[0].ShortTitle+","+data.count+","+data.totalCost+","+data.avgTotalCost+","+data.stdDevTotalCost+","+data.call+","+data.avgCall
+         +","+data.stdDevCall
+        
+         );
+});
+
+
