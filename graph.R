eb918a00325c25a1d38ee0f724ceb8cd1af88a82
Sebastien DIAZ <sebastien.diaz@gmail.com>
2016-07-18 20:56:36 +0200

    add r

--- graph.R
+++ graph.R
@@ -1,13 +1,23 @@
 programmeDateAgreg <- read.csv("programmeDateAgreg.csv")
-programmeDateAgreg<-programmeDateAgreg[order(programmeDateAgreg$totalCost,programmeDateAgreg$X_id.programme, programmeDateAgreg$X_id.year),]
+programmeDateAgreg<-programmeDateAgreg[order(programmeDateAgreg$X_id.programme, programmeDateAgreg$X_id.year),]
 programmes<-programmeDateAgreg$X_id.programme
-png("programmeDateAgreg.png",width = 1200,height = 1000)
-plot(0, 0, xlim = c(2006,2021), ylim = range(programmeDateAgreg$totalCost), type = "n", col=programmes)
-legend(2006,6500000000,unique(programmes),col=1:length(programmes),pch=1)
+png("programmeDateAgregTotal.png",width = 800,height = 600)
+plot(0, 0, xlim = c(2006,2021), ylim = range(programmeDateAgreg$totalCost/1000000), type = "n", col=programmes)
+legend(2006,7500,unique(programmes),col=1:length(programmes),pch=1)
 cole<-1
 for(prog in levels(programmeDateAgreg$X_id.programme)){
   data<-programmeDateAgreg[programmeDateAgreg$X_id.programme==prog,]
-  lines(data$totalCost ~ data$X_id.year,title=prog,col=cole)
+  lines(data$totalCost/1000000 ~ data$X_id.year,title=prog,col=cole)
+  cole<-cole+1
+}
+dev.off()
+png("programmeDateAgregNbProject.png",width = 800,height = 600)
+plot(0, 0, xlim = c(2006,2021), ylim = range(programmeDateAgreg$count), type = "n", col=programmes)
+legend(2006,6500,unique(programmes),col=1:length(programmes),pch=1)
+cole<-1
+for(prog in levels(programmeDateAgreg$X_id.programme)){
+  data<-programmeDateAgreg[programmeDateAgreg$X_id.programme==prog,]
+  lines(data$count ~ data$X_id.year,title=prog,col=cole)
   cole<-cole+1
 }
 dev.off()
