8fec13b2a8508b1065360289164effeb87736ec1
Sebastien DIAZ <sebastien.diaz@gmail.com>
2016-08-06 10:40:59 +0200

    Panez

--- sparkYear.scala
+++ sparkYear.scala
@@ -9,7 +9,7 @@ import com.mongodb.hadoop.{
 import com.mongodb.hadoop.io.MongoUpdateWritable
 
 import java.io._
-
+import java.util._
 import com.cloudera.datascience.lsa._
 import com.cloudera.datascience.lsa.ParseWikipedia._
 import com.cloudera.datascience.lsa.RunLSA._
@@ -20,7 +20,6 @@ import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import breeze.linalg.{DenseMatrix => BDenseMatrix, DenseVector => BDenseVector, SparseVector => BSparseVector}
 import org.apache.spark.mllib.regression._
 import org.apache.spark.rdd._
-import com.mongodb.BasicDBList
 
 
 @transient val mongoConfig = new Configuration()
@@ -32,42 +31,25 @@ val documents = sc.newAPIHadoopRDD(
     classOf[Object],            // Key type
     classOf[BSONObject])        // Value type
 
-//:type documents 
-val stopWords = sc.broadcast(ParseWikipedia.loadStopWords("deps/lsa/src/main/resources/stopwords.txt")).value
-var lemmatized = documents.map(s=> (s._2.get("_id").toString,ParseWikipedia.plainTextToLemmas(s._2.get("objective").toString, stopWords, ParseWikipedia.createNLPPipeline()),s._2.get("years").asInstanceOf[BasicDBList]))
-.filter(_._2.size > 1).flatMap(a=>{
-    var sized=1;
-    if(a._3!=null){
-        sized=a._3.size
-    }
-    var z = new Array[((String, Seq[String], String))](sized)
-    if(a._3!=null&&a._3.size>0){
-      for ( i  <- 0 to a._3.size-1 ) {
-         z(i)=(a._1,a._2,a._3.get(i).toString)
-      }
-    }else{
-        z = new Array[((String, Seq[String], String))](1)
-        z(0)=(a._1,a._2,"")
-    }
-    
-    z
-        
-}).groupBy(a => a._3)
 
+var stopWords = sc.broadcast(ParseWikipedia.loadStopWords("deps/lsa/src/main/resources/stopwords.txt")).value
+
+for( year <- 2006 to 2022){
+var select=documents.filter(a=>a._2.get("years").asInstanceOf[List[Double]].contains(year.asInstanceOf[Double]))
+    println( year+":"+select.count())
+    if(select.count()>0){
+        
+var lemmatized = documents.filter(a=>a._2.get("years").asInstanceOf[List[String]].contains(year)).map(s=> (s._2.get("_id").toString,ParseWikipedia.plainTextToLemmas(s._2.get("objective").toString, stopWords, ParseWikipedia.createNLPPipeline())))
 val numTerms = 1000
 val k = 100 // nombre de valeurs singuliers à garder
 val nbConcept = 30
 
-
-var documentSize=documents.collect().length
+val filtered = lemmatized.filter(_._2.size > 1)
+val documentSize=select.count().asInstanceOf[Int]
 println("Documents Size : "+documentSize)
 println("Number of Terms : "+numTerms)
-var fullMap=lemmatized.map(yearBased=>{
-    var filtered=sc.parallelize(yearBased._2.toSeq).map(a=>(a._1,a._2))
-    var documentSize=yearBased._2.size
-    val (termDocMatrix, termIds, docIds, idfs) = ParseWikipedia.termDocumentMatrix(filtered, stopWords, numTerms, sc)
+val (termDocMatrix, termIds, docIds, idfs) = ParseWikipedia.termDocumentMatrix(filtered, stopWords, numTerms, sc)
 
-    var outputConfig = new Configuration()
 
   val mat = new RowMatrix(termDocMatrix)
 
@@ -98,9 +80,9 @@ var fullMap=lemmatized.map(yearBased=>{
 
 var docConceptRDD=sc.parallelize(docConcept.toSeq)
   
-var toWrite=docConceptRDD.map(a => ((a._1,yearBased._1), a._2.toArray))
+var toWrite=docConceptRDD.map(a => ((a._1,year), a._2.toArray))
 
- outputConfig = new Configuration()
+var outputConfig = new Configuration()
   outputConfig.set("mongo.output.uri",
     "mongodb://localhost:27017/cordir.projetDocConceptYear")
 toWrite.saveAsNewAPIHadoopFile(
@@ -132,8 +114,9 @@ for ( a <- topConceptTerms) {
 var parr=sc.parallelize(termConcept.toSeq)
 outputConfig = new Configuration()
 outputConfig.set("mongo.output.uri","mongodb://localhost:27017/cordir.projetTermConceptYear")
-parr.map(a => ((a._1,yearBased._1),a._2.toArray)).coalesce(1,true).saveAsNewAPIHadoopFile("file:///this-is-completely-unused",classOf[Object],classOf[BSONObject],classOf[MongoOutputFormat[Object, BSONObject]],outputConfig)
-})
+parr.map(a => ((a._1,year),a._2.toArray)).coalesce(1,true).saveAsNewAPIHadoopFile("file:///this-is-completely-unused",classOf[Object],classOf[BSONObject],classOf[MongoOutputFormat[Object, BSONObject]],outputConfig)
+    }
+    }
   
 
 exit
