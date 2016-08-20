573e8420b0567f1f2db817366014c7757dc3f10d
Sebastien DIAZ <sebastien.diaz@gmail.com>
2016-08-20 15:13:38 +0200

    elegant

---
+++ sparkLaunch.bat
@@ -0,0 +1,1 @@
+spark-1.6.2-bin-hadoop2.6/bin/spark-shell --driver-memory 2G --executor-memory 3G --jars mongodb-driver-3.3.0.jar,mongo-hadoop-core-1.5.2.jar,mongo-java-driver-3.3.0.jar,deps/lsa/target/lsa-1.0.0-jar-with-dependencies.jar 
